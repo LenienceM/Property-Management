f2180a3f382e06bc180da5e6da2d1d3cab694b7f
Lenience Moyo <Lenience.moyo@gmail.com>
2026-06-27 22:25:43 +0200

    fix: updated footer file

--- property-management/frontend/src/components/layout/PageLayout.tsx
+++ property-management/frontend/src/components/layout/PageLayout.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
-import {Navbar} from "./Navbar";
-//import Footer from "./Footer";
-
+import { Navbar } from "./Navbar";
+import PageFooter from "./PageFooter"; 
 
 type Props = {
   children: ReactNode;
@@ -12,10 +11,13 @@ export default function PageLayout({ children }: Props) {
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Navbar />
 
+      {/* flex-1 pushes everything below this down to the bottom */}
       <main className="flex-1">
         {children}
       </main>
 
+      {/* Add footer */}
+      <PageFooter />
     </div>
   );
 }
